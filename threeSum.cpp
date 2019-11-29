355abb6d99ec65a6c7801ff933aab321ed987fa7
ngmj <373094688@qq.com>
2019-11-29 11:46:52 +0800

    1 add threeSum.cpp （https://leetcode-cn.com/problems/3sum/）

--- threeSum.cpp
+++ threeSum.cpp
@@ -1,46 +1,78 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include <unordered_map>
 #include "string_util.h"
 
 /*
- * https://leetcode-cn.com/problems/two-sum/
- * 1轮 hash表存储target-num[i]
- * 2轮 遍历num是否存在于hash表中，存在则表明有该值
+ * https://leetcode-cn.com/problems/3sum/ 
+ * sort 数组
+ * 挑选中间数，然后左后双指针 
  * */
 using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-        res.reserve(2);
-        unordered_map<int, int> pair2Idx;
-        pair2Idx.reserve( nums.size() );
-        for ( size_t i = 0; i < nums.size(); i++ )
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        if ( nums.size() < 3 )
         {
-            pair2Idx.insert( make_pair(target - nums[i], i) );
+            return res;
         }
+        std::sort( nums.begin(), nums.begin() + nums.size() );
 
-        for ( size_t i = 0; i < nums.size(); i++ )
+        for ( size_t i = 0; i < nums.size() - 2; i++ )
         {
-            auto iter = pair2Idx.find( nums[i] );
-            if ( pair2Idx.end() != iter 
-                    && iter->second != i )
+            size_t first = i + 1;
+            size_t last = nums.size() - 1;
+            while ( first < last )
             {
-                res.push_back( i );
-                res.push_back( iter->second );
-                break;
+                // 三个数同正同负，则退出
+                if ( nums[i] > 0 || nums[last] < 0 )
+                {
+                    break;
+                }
+                int sum = nums[i] + nums[first] + nums[last];
+                if ( 0 == sum )
+                {
+                    res.push_back( { nums[i], nums[first], nums[last] } );
+                    while ( first < last && nums[first] == nums[++first] ) { }
+                } else if ( sum > 0 )
+                {
+                    while ( first < last && nums[last] == nums[--last] ) { }
+                } else
+                {
+                    while ( first < last && nums[first] == nums[++first] ) { }
+                }
             }
+            while ( i < nums.size() - 2 && nums[i] == nums[i+1] ) { i++; }
         }
         return res;
     }
 };
 
-int main()
+void Sol( vector<int> &nums )
 {
     Solution sol;
-    vector<int> nums1 = { 2, 7, 11, 15 };
-    int target1 = 9;
-    cout << VectorToString(nums1) << " - " << target1 << "|" << VectorToString(sol.twoSum( nums1, target1 )) << endl;
+    vector<vector<int>> res = sol.threeSum( nums );
+    cout << VectorToString(nums);
+    for ( auto &vec: res )
+    {
+        cout << " | " << VectorToString( vec );
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> nums1 = { -1, 0, 1, 2, -1, -4 };
+    Sol( nums1 );
+
+    vector<int> nums2 = { 0, 0, 0, 0, 0 };
+    Sol( nums2 );
+
+    vector<int> nums3 = {};
+    Sol( nums3 );
+
+    return 0;
 }
