e6377d2463e1d9b189daf20452507b8531a933a5
ngmj <373094688@qq.com>
2020-08-27 11:56:30 +0800

    1 提交若干题

--- letterCombinations_17.cc
+++ letterCombinations_17.cc
@@ -3,12 +3,14 @@
 #include <iostream>
 #include <algorithm>
 #include <map>
+#include <list>
 #include <set>
 #include <stack>
 #include <queue>
 #include <deque>
 #include <unordered_set>
 #include <unordered_map>
+#include "serialize_deserialize_tree.hpp"
 #include "string_util.h"
 
 /*
@@ -18,14 +20,12 @@
  *  std::priority_queue<int, std::vector<int>, std::greater<int> > 小根堆
  * queue: front(), back(), push(), pop(), empty()
  * deque: front(), back(), push_back(), pop_back, push_front, pop_front(), empty(), clear()
+ * list: push_back, push_front, insert(pos, val), front, remove(val), erase(iter)
  *
  * iter = lower_bound(start, end, val) 返回[start, end)>=val的第一个位置（下界）
  * iter = upper_bound(start, end, val) 返回[start, end)>val的第一个位置（上界）
  * 如(11,12) lower_bound查找11，返回0；upper_bound则返回1
- * https://leetcode-cn.com/problems/maximum-subarray/
- * 分治法
- * 动态规划法
- * 贪心法（实际就是动态规划法+状态压缩）
+ * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
  * */
 using namespace std;
 
@@ -39,28 +39,44 @@ LL MIN_LL = std::numeric_limits<LL>::min();
 int MAX_INT = std::numeric_limits<int>::max();
 int MIN_INT = std::numeric_limits<int>::min();
 
+const unordered_map<int, vector<char>> m = {
+  {2, {'a', 'b', 'c'}},
+  {3, {'d', 'e', 'f'}},
+  {4, {'g', 'h', 'i'}},
+  {5, {'j', 'k', 'l'}},
+  {6, {'m', 'n', 'o'}},
+  {7, {'p', 'q', 'r', 's'}},
+  {8, {'t', 'u', 'v'}},
+  {9, {'w', 'x', 'y', 'z'}}};
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int max_val = nums[0];
-        // dp[i] = max(dp[i-1] + val, val) 以i为结尾的最大子序列
-        // 状态压缩：sum=max(sum + val, val)
-        int sum = nums[0]; 
-        for (int i = 1; i < nums.size(); ++i) {
-            int val = nums[i];
-            sum += val;
-            if (sum < val) {
-                sum = val;
-            } 
-            max_val = max(sum, max_val);
-        }
-        return max_val;
+  vector<string> letterCombinations(const string &digits) {
+    string s;
+    s.resize(digits.size());
+    dfs(s, digits, 0);
+    return ans;
+  }
+
+  void dfs(string s, const string &digits, int i) {
+    if (i > digits.size() - 1) {
+      ans.push_back(s.c_str());
+      return;
     }
+    int c = digits[i] - '0';
+    auto iter = m.find(c);
+    if (m.end() == iter) { return; }
+    for (auto &t: iter->second) {
+      s[i] = t;
+      dfs(s, digits, i+1);
+    }
+  }
+private:
+  vector<string> ans;
 };
 
 int main() {
     Solution sol;
-    vector<int> n1 = {-2,1,-3,4,-1,2,1,-5,4};
-    cout << sol.maxSubArray(n1) << endl;
+    cout << (sol.letterCombinations("23") == 
+          vector<string>({"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"})) << endl;
     return 0;
 }
